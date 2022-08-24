c703f468ef45cb1c9978c129b46b6a4c840cbc36
Seniatical <isaja7710@gmail.com>
2022-08-24 19:05:02 +0100

    Add basic methods for fetching angles

--- cake/geometry/basic.py
+++ cake/geometry/basic.py
@@ -1,7 +1,8 @@
 # A base ABC class for defining geometrical objects
-from abc import ABC, abstractproperty, abstractmethod
-from typing import Dict
+from abc import ABC, abstractmethod
+from typing import Dict, List
 from string import ascii_lowercase
+from itertools import permutations
 
 
 def _get_numerical_key(_data: dict) -> int:
@@ -10,13 +11,16 @@ def _get_numerical_key(_data: dict) -> int:
         return -1
 
     keys = _data.keys()
-    sorted_keys = sorted(keys, key=lambda key: ascii_lowercase.index(key))
-    return ascii_lowercase.index(sorted_keys[-1])
+    return ascii_lowercase.index(list(keys)[-1])
 
 
 class Shape(ABC):
     r"""
     Base object for generating shapes, or shape types
+
+    .. note::
+        If any lengths/angles are set to 0,
+        Cake will assume that the value is unknown.
 
     Parameters
     ----------
@@ -41,16 +45,32 @@ class Shape(ABC):
                 >>> Shape(5, 12, 13, angles={"BAC": 30, "ABC": 60, "BCA": 90})
                 >>> ... # Valid shape
     """
+    def _clean_angles(self) -> None:
+        if any(i for i in self.angles.values() if not 0 < i < 360):
+            raise ValueError("Angles must be greater then 0 and less then 360")
+
+        angles = self.angles.copy()
+        combinations = self.possible_angles()
+        markers = []
+
+        for angle, value in angles.items():
+            self.angles.pop(angle)
+
+            if len(angle) != 3:
+                raise ValueError(f"Invalid angle {angle}")
+            if angle.upper() not in combinations:
+                raise ValueError(f"Unknown points in angle {angle}")
+            if angle[1] not in markers:
+                markers.append(angle[1])
+                self.angles[angle.upper()] = value
+
     def __init__(
         self, *r_lengths,
-        lengths: Dict[str, float] = {},
-        angles: Dict[str, float] = {},
+        lengths: Dict[str, float] = None,
+        angles: Dict[str, float] = None,
     ) -> None:
-        self.lengths = lengths
-        self.angles = angles
-
-        if any(i for i in self.angles.values() if not (0 < i < 360)):
-            raise ValueError("Angles in shape must be greater then 0, and less then 360")
+        self.lengths = lengths or dict()
+        self.angles = angles or dict()
 
         if r_lengths:
             curr = _get_numerical_key(self.lengths)
@@ -59,6 +79,35 @@ class Shape(ABC):
                 self.lengths[ascii_lowercase[curr]] = v
 
         self.sides = len(self.lengths)
+        self._clean_angles()
+
+    def possible_angles(self) -> List[str]:
+        """Returns all possible angle combinations in the shape"""
+        angles = permutations(self.lengths, 3)
+
+        return [''.join(angle).upper() for angle in angles]
+
+    def get_angle(self, angle: str) -> List[str]:
+        """Get an angle assigned from :attr:`Shape.angles`
+
+        Parameters
+        ----------
+        angle: :class:`str`
+            Angle to search for
+        """
+        if angle.upper() not in self.possible_angles():
+            raise ValueError("Unknown angle")
+
+        marker = angle[1]
+        similar_permutation = None
+        for angle in self.angles:
+            if angle[1] == marker:
+                similar_permutation = angle
+                break
+        
+        if not similar_permutation:
+            return 0
+        return self.angles[similar_permutation]
 
     @abstractmethod
     def set_angle(self, angle: str, size: float) -> float:
