4a97b09a163bc9d846dd2a0a2d7e0c95edd052ed
Seniatical <isaja7710@gmail.com>
2022-08-24 15:38:29 +0100

    Add docstrings

--- cake/geometry/polygon/base.py
+++ cake/geometry/polygon/base.py
@@ -9,6 +9,7 @@ class Polygon(Shape):
     ----------
     Shares same parameters as :class:`Shape`
     """
+
     def __init__(self, *lengths, **kwds) -> None:
         super().__init__(*lengths, **kwds)
 
@@ -21,6 +22,14 @@ class Polygon(Shape):
         side = self.lengths[list(self.lengths)[0]]
         return (self.sides * side * self.apothem_from_crad()) / 2
 
+    def set_angle(self, angle: str, size: float) -> None:
+        if not 0 < size < 360:
+            raise ValueError("Angle must be greater then 0, and less then 360")
+
+        if any(i for i in angle if i not in self.lengths):
+            raise ValueError("Unknown points in angle")
+        self.angles[angle] = size
+
     def perimeter(self) -> float:
         return sum(self.lengths.values())
 
@@ -28,32 +37,70 @@ class Polygon(Shape):
         return (self.sides - 2) * 180
 
     def triangles(self) -> int:
+        """ Returns the number of triangle that can be fitted in the regular polygon """
         return self.sides - 2
 
     def exterior_angle(self) -> float:
-        assert self.is_regular(), "Exterior angle can only be worked out for regular polygons"
+        """ Return size of exterior angle for regular polygons """
         return 360 / self.sides
 
     def interior_angle(self) -> float:
-        assert self.is_regular(), "Interior angle can only be worked out for regular polygons"
+        """ Returns size of interior polygon for regular polygons """
         return 180 - (360 / self.sides)
 
     def circumradius(self, *, side_index: int = 0) -> float:
+        """ Returns the circumradius for polygone
+
+        Parameters
+        ----------
+        size_index: :class:`int`
+            Index of side to use
+
+            .. rubric:: Example
+                .. code-block::py
+                    >>> p = Polygon(3, 3, 3, 3)
+                                    ^  ^  ^  ^
+                                    a  b  c  d
+                    >>> p.circumradius(side_index=2)
+                    # Uses side c
+        """
         side = self.lengths[list(self.lengths)[side_index]]
 
         return side / (2 * sin(pi / self.sides))
 
     def apothem_from_length(self, *, side_index: int = 0) -> float:
+        """ Return apothem using a side of polygon
+
+        Parameters
+        ----------
+        side_index: :class:`int`
+            Index of side to use, refer to :meth:`cake.geometry.Polygon.circumradius` for usage.
+        """
         side = self.lengths[list(self.lengths)[side_index]]
 
         return (side / (2 * tan(pi / self.sides)))
 
-    def apothem_from_crad(self):
-        c_rad = self.circumradius()
+    def apothem_from_crad(self, *, side_index: int = 0):
+        """ Return apothem using the circumradius of polygon
+
+        Parameters
+        ----------
+        side_index: :class:`int`
+            Index of side to use, refer to :meth:`cake.geometry.Polygon.circumradius` for usage.
+        """
+        c_rad = self.circumradius(side_index=side_index)
         return c_rad * cos(pi / self.sides)
 
-
     def is_regular(self) -> bool:
+        """Checks whether the polygon is regular.
+
+        * **All** sides are the same
+        * **All** angles are the same.
+
+        .. note::
+            If there are no angles provided, it will skip the check for angles,
+            assuming there all the same.
+        """
         r = sum(self.lengths.values()) % (self.lengths[list(self.lengths)[0]])
 
         if self.angles:
@@ -64,9 +111,6 @@ class Polygon(Shape):
         if r == 0 and a == 0:
             return True
         return False
-
-    def set_angle(self, angle: str, size: float) -> None:
-        ...
 
     def __repr__(self) -> str:
         sides = [f"{k}={v}" for k, v in self.lengths.items()]
