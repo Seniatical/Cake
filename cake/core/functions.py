e3c6f74371dca912148b4118b1d0fcb54fab5c98
Seniatical <isaja7710@gmail.com>
2023-07-22 19:12:11 +0100

    Add inverse trignometric functions

--- cake/core/functions.py
+++ cake/core/functions.py
@@ -128,6 +128,42 @@ class Tan(Function):
         return cake.Real(tan(v))
 
 
+class ASin(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(asin(v))
+
+
+class ACos(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(acos(v))
+
+
+class ATan(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(atan(v))
+
+
+class ATan2(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(atan2(v))
+
+
 class SinH(Function):
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
@@ -153,6 +189,33 @@ class TanH(Function):
         if opts.get('prehandle'):
             v = self.prehandler(v)
         return cake.Real(tanh(v))
+
+
+class ASinH(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(asinh(v))
+
+
+class ACosH(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(acosh(v))
+
+
+class ATanH(Function):
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = cake.to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        return cake.Real(atanh(v))
 
 
 class Truncate(Function):
