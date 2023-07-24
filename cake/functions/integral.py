b62d84aa3c16d019c93e060f507795340a7ad791
Seniatical <isaja7710@gmail.com>
2023-07-24 12:40:57 +0100

    Add round function

--- cake/functions/integral.py
+++ cake/functions/integral.py
@@ -11,7 +11,10 @@ class Truncate(Function):
             v = to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return Real(trunc(v))
+        try:
+            return Real(trunc(v))
+        except Exception:
+            return Truncate(v)
 
 
 class Ceil(Function):
@@ -20,7 +23,10 @@ class Ceil(Function):
             v = to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return Real(ceil(v))
+        try:
+            return Real(ceil(v))
+        except Exception:
+            return Ceil(v)
 
 
 class Floor(Function):
@@ -29,4 +35,31 @@ class Floor(Function):
             v = to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return Real(floor(v))
+        try:
+            return Real(floor(v))
+        except Exception:
+            return Floor(v)
+
+
+class Round(Function):
+    def __init__(self, parameter: Any, coefficient: Any = 1, power: Any = 1, n_places: int = 2) -> None:
+        super().__init__(parameter, coefficient, power)
+        self.n_places = n_places
+
+    def __repr__(self) -> str:
+        return f'Round(parameter={repr(self.parameter)}, coefficient={repr(self.coefficient)}, power={repr(self.power)}, n_places={repr(self.n_places)})'
+
+    def __str__(self) -> str:
+        s = super().__str__()[:-1]
+        return s + f', {self.n_places})'
+
+    def _handler(self, v, **opts) -> Any:
+        if opts.get('rad'):
+            v = to_radians(v)
+        if opts.get('prehandle'):
+            v = self.prehandler(v)
+        
+        try:
+            return Real(round(v, self.n_places))
+        except Exception:
+            return Round(v)
