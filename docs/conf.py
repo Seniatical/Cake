768f86625fb45d93317d2199fff999754782d72c
Seniatical <isaja7710@gmail.com>
2023-07-24 19:48:31 +0100

    Add more to homepage

--- docs/conf.py
+++ docs/conf.py
@@ -25,43 +25,41 @@ intersphinx_mapping = {
 }
 
 templates_path = ['_templates']
-
 exclude_patterns = ['*.md', '*.template']
-
 
 html_theme = 'pydata_sphinx_theme'
 html_logo = "_static/icon.png"
 
 html_theme_options = {
    "favicons": [
-      {
-         "rel": "icon",
-         "sizes": "16x16",
-         "href": "icon.png",
-      },
-      {
-         "rel": "icon",
-         "sizes": "32x32",
-         "href": "icon.png",
-      },
-      {
-         "rel": "apple-touch-icon",
-         "sizes": "180x180",
-         "href": "icon.png"
-      },
-   ],
+        {
+            "rel": "icon",
+            "sizes": "16x16",
+            "href": "icon.png",
+        },
+        {
+            "rel": "icon",
+            "sizes": "32x32",
+            "href": "icon.png",
+        },
+        {
+            "rel": "apple-touch-icon",
+            "sizes": "180x180",
+            "href": "icon.png"
+        },
+    ],
 
    "icon_links": [
-      {
-         "name": "GitHub",
-         "url": "https://github.com/Seniatical/Cake",
-         "icon": "fab fa-github",
-      },
-      {
-         "name": "Discord",
-         "url": "https://discord.com/invite/Q5mFhUM",
-         "icon": "fab fa-discord"
-      },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/Seniatical/Cake",
+            "icon": "fab fa-github",
+        },
+        {
+            "name": "Discord",
+            "url": "https://discord.gg/BPC3STaTRd",
+            "icon": "fab fa-discord"
+        },
     ],
 
    "use_edit_page_button": True,
@@ -69,7 +67,14 @@ html_theme_options = {
    "show_prev_next": False,
    "navigation_depth": 3,
    "search_bar_text": "Search the docs ...",
-   "footer_items": ["copyright", "last-updated"],
+   "footer_start": ["last-updated"],
+   "footer_end": ["copyright"],
+
+   "announcement": "Cake is currently in its early stages, errors may be expected! If you happen to encounter any please report <a href='https://github.com/Seniatical/Cake/Issues'>here</a>",
+}
+
+html_sidebars = {
+    "**": ["sidebar-nav-bs", "search-field"]
 }
 
 html_context = {
@@ -81,15 +86,7 @@ html_context = {
     "last_updated": datetime.datetime.utcnow().strftime('%d/%m/%Y'),
 }
 
-html_sidebars = {
-    "**": ["search-field", "sidebar-nav-bs"],
-}
-
 html_static_path = ['_static']
-html_css_files = [
-    'css/style.css',
-    'css/codeblocks.css'
-]
 
 html_title = "Cake"
 
--- docs/index.rst
+++ docs/index.rst
@@ -3,13 +3,38 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to cake's documentation!
-================================
+********************************
+Welcome to Cake's documentation!
+********************************
+Welcome to the cake library, a powerful OOP based mathmatical library for python.
+
+.. code-block:: py
+
+   from cake import *
+
+   x = Variable('x')
+   expr = Sqrt(x + 5)
+
+   print(expr.solve(x=20))
+   # 5
+
+
+Getting Started 
+===============
+To get started with cake, we need you to install it first.
+
+.. code-block:: sh
+
+   ## Windows
+   pip install git+https://github.com/Seniatical/Cake
+
+   ## Linux/Mac
+   pip3 install git+https://github.com/Seniatical/Cake
+
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
 
 
 Indices and tables
