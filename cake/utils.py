336aec769088a6d7c41c1f8f91eb3cd78eccbc0a
Seniatical <isaja7710@gmail.com>
2023-07-24 12:39:05 +0100

    utility for solving either functions or expressions

--- cake/utils.py
+++ cake/utils.py
@@ -9,3 +9,11 @@ def to_radians(x: Any) -> Any:
 
 def to_degrees(x: Any) -> Any:
     return x / (math.pi / 180)
+
+
+def solve_if_possible(x: Any, **kwds) -> Any:
+    if hasattr(x, 'solve'):
+        return x.solve(**kwds)
+    elif hasattr(x, 'evaluate'):
+        return x.evaluate(**kwds)
+    return x
