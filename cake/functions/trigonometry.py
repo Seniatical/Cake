ab0cdac0056c184c1fee35a61667ca1f132fd0b9
Seniatical <isaja7710@gmail.com>
2023-07-26 13:26:41 +0100

    Add basic docstrings to trig functions

--- cake/functions/trigonometry.py
+++ cake/functions/trigonometry.py
@@ -6,117 +6,186 @@ import cake
 from math import *
 
 class Sin(Function):
+    ''' Sin function ''' 
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(sin(v))
-
+        
+        try:
+            return cake.Real(sin(v))
+        except Exception:
+            return Sin(v)
 
 class Cos(Function):
+    ''' Cos function '''
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(cos(v))
+
+        try:
+            return cake.Real(cos(v))
+        except Exception:
+            return Cos(v)
 
 
 class Tan(Function):
+    ''' tan function '''
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(tan(v))
+
+        try:
+            return cake.Real(tan(v))
+        except Exception:
+            return Tan(v)
 
 
 class ASin(Function):
+    ''' arc sin or inverse sin function '''
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(asin(v))
+
+        try:
+            return cake.Real(asin(v))
+        except Exception:
+            return ASin(v)
 
 
 class ACos(Function):
+    ''' arc cos or inverse cos function '''
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(acos(v))
+        try:
+            return cake.Real(acos(v))
+        except Exception:
+            return ACos(v)
 
 
 class ATan(Function):
+    ''' arc tan or inverse tan function '''
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(atan(v))
+
+        try:
+            return cake.Real(atan(v))
+        except Exception:
+            return ATan(v)
 
 
 class ATan2(Function):
+    ''' arc tan2 or inverse tan2 function '''
+
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(atan2(v))
+
+        try:
+            return cake.Real(atan2(v))
+        except Exception:
+            return ATan2(v)
 
 
 class SinH(Function):
+    ''' Hyperbolic sin function '''
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(sinh(v))
+        try:
+            return cake.Real(sinh(v))
+        except Exception:
+            return SinH(v)
 
 
 class CosH(Function):
+    ''' Hyperbolic cos function '''
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(cosh(v))
+
+        try:
+            return cake.Real(cosh(v))
+        except Exception:
+            return CosH(v)
 
 
 class TanH(Function):
+    ''' Hyperbolic tan function '''
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(tanh(v))
+
+        try:
+            return cake.Real(tanh(v))
+        except Exception:
+            return TanH(v)
 
 
 class ASinH(Function):
+    ''' Arc hyperbolic sin or inverse hyperbolic sin function '''
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(asinh(v))
+
+        try:
+            return cake.Real(asinh(v))
+        except Exception:
+            return ASinH(v)
 
 
 class ACosH(Function):
+    ''' Arc hyperbolic cos or inverse hyperbolic cos function '''
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(acosh(v))
+
+        try:
+            return cake.Real(acosh(v))
+        except Exception:
+            return ACosH(v)
 
 
 class ATanH(Function):
+    ''' Arc hyperbolic tan or inverse hyperbolic tan function '''
     def _handler(self, v, **opts) -> Any:
         if opts.get('rad'):
             v = cake.to_radians(v)
         if opts.get('prehandle'):
             v = self.prehandler(v)
-        return cake.Real(atanh(v))
+
+        try:
+            return cake.Real(atanh(v))
+        except Exception:
+            return ATanH(v)
