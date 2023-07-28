39c06d17684ea0476f19456122ca421e510e494f
Seniatical <isaja7710@gmail.com>
2023-07-28 22:09:52 +0100

    Improve documentation for points

--- cake/geometry/__init__.py
+++ cake/geometry/__init__.py
@@ -1,7 +1,2 @@
-from . import (
-    core,
-    points
-)
-
 from .core import GeometryO
 from .points import Point2D
--- cake/geometry/points.py
+++ cake/geometry/points.py
@@ -6,12 +6,26 @@ from .core import GeometryO
 
 
 class Point2D(GeometryO):
+    ''' Represents a co-ordinate on a 2D plane, meaning it only has 2 values **x** and **y**.
+
+    Parameters
+    ----------
+    x, y: Any[Like[:class:`cake.BasicNode`]]
+        Value for co-ordinate
+    '''
     def __init__(self, x: Any, y: Any) -> None:
         self.x = x
         self.y = y
 
     @classmethod
     def from_tuple(cls, tup: Tuple[Any, Any]) -> Point2D:
+        ''' Generates a 2D point from a tuple
+
+        Parameters
+        ----------
+        tup: Tuple[Any[Like[:class:`cake.BasicNode`]], Any[Like[:class:`cake.BasicNode`]]]
+            Tuple to create point from
+        '''
         return Point2D(*tup)
 
     def as_tuple(self) -> Tuple[Any, Any]:
