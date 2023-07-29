0ac67328e596b72244bf1d65a7074216894c2bab
Seniatical <isaja7710@gmail.com>
2023-07-29 12:34:52 +0100

    Add core objects

---
+++ cake/expressions/core.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from typing import Any, Tuple
+from abc import *
+from cake._abc import Like, BasicExpression
+from cake import Expression
+
+
+class ExpressionBase(BasicExpression, Like[Expression], ABC):
+
+    @abstractmethod
+    def solve(self, **kwds) -> Any:
+        ''' Solves the expression using given values, parameters may vary '''
+
+    @abstractmethod
+    def r_solve(self, **kwds) -> Any:
+        ''' Solves expression when the result is known, parameters may vary '''
+
+    @abstractclassmethod
+    def generic(cls) -> ExpressionBase:
+        ''' Returns generic version of expression '''
+
+    @abstractclassmethod
+    def from_expression(cls, expr: Expression) -> ExpressionBase:
+        ''' Creates expression from :class:`Expression` '''
+
+
+class PolynomialExpression(ExpressionBase):
+
+    @abstractmethod
+    def differentiate(self) -> Any:
+        ''' Differentiates a polynomial expression '''
+
+    @abstractmethod
+    def integrate(self) -> Any:
+        ''' Integrates a polynomial expression '''
+
+    @abstractmethod
+    def roots(self) -> Tuple[Any, ...]:
+        ''' Returns roots of polynomial '''
